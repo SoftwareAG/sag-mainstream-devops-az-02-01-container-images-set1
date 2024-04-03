95045d944db74343bd2bcbba8d4c2cc991e42b67
miunsag <108091043+miunsag@users.noreply.github.com>
2024-04-03 18:22:18 +0200

    try to remove the second CR login, probably redundant

--- common/5.pushOutput/01.pushTags.sh
+++ common/5.pushOutput/01.pushTags.sh
@@ -5,22 +5,22 @@
 . ./setEnv.sh
 . "${SUIF_CACHE_HOME}/01.scripts/commonFunctions.sh"
 
-logI "Getting service principal credentials..."
-if [ ! -f "${ACRSPCREDENTIALS_SECUREFILEPATH}" ]; then
-  logE "Secure file path not present: ${ACRSPCREDENTIALS_SECUREFILEPATH}"
-  exit 1
-fi
+# logI "Getting service principal credentials..."
+# if [ ! -f "${ACRSPCREDENTIALS_SECUREFILEPATH}" ]; then
+#   logE "Secure file path not present: ${ACRSPCREDENTIALS_SECUREFILEPATH}"
+#   exit 1
+# fi
 
-chmod u+x "${ACRSPCREDENTIALS_SECUREFILEPATH}"
-. "${ACRSPCREDENTIALS_SECUREFILEPATH}"
+# chmod u+x "${ACRSPCREDENTIALS_SECUREFILEPATH}"
+# . "${ACRSPCREDENTIALS_SECUREFILEPATH}"
 
-if [ -z ${AZ_ACR_SP_ID+x} ]; then
-  logE "Secure information has not been sourced correctly"
-  exit 2
-fi
+# if [ -z ${AZ_ACR_SP_ID+x} ]; then
+#   logE "Secure information has not been sourced correctly"
+#   exit 2
+# fi
 
-logI "Logging in to repository ${MY_AZ_ACR_URL}"
-buildah login -u "${AZ_ACR_SP_ID}" -p "${AZ_ACR_SP_SECRET}" "${MY_AZ_ACR_URL}" || exit 1
+# logI "Logging in to repository ${MY_AZ_ACR_URL}"
+# buildah login -u "${AZ_ACR_SP_ID}" -p "${AZ_ACR_SP_SECRET}" "${MY_AZ_ACR_URL}" || exit 1
 
 logI "Pushing tag ${JOB_CONTAINER_MAIN_TAG}"
 buildah push "${JOB_CONTAINER_MAIN_TAG}" || exit 2
